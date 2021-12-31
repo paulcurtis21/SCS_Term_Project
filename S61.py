db61dcc76e6d99401138ed6b6fe6eca96333844e
paulcurtis21 <93895962+paulcurtis21@users.noreply.github.com>
2021-12-31 17:34:10 -0500

    Update S61.py

--- S61.py
+++ S61.py
@@ -153,3 +153,126 @@ for i in range(8):
     [t, x_, y_] = RK4_S61(2, 1/6, 1/5, 100, 10000, x_0[i], y_0[i])
     plt.plot(x_, y_, color = 'black', linewidth = 2)
 S61(y, x, R, delta, lam)
+
+#%%
+
+def S61B(y, x, R, delta, lam_vals):
+    """
+    y is nondimensional temperature; x is nondimensional salinity.
+    R, delta and lambda are parameters which need to be specified at the start.
+    The sign of the flow always needs to be assessed.
+    """
+
+    lam = np.linspace(0.01, 1, lam_vals)
+    
+    F_stable_node = np.zeros(len(lam))
+    F_stable_spiral = np.zeros(len(lam))
+    F_unstable_node = np.zeros(len(lam))
+    
+    # Define the right hand side of the two time derivatives
+    def f(y, x, lam, R, delta): # x dot
+        f = delta*(1 - x) - (x/lam)*np.abs(-y + R*x)
+        return f
+
+    def g(y, x, lam, R): # y dot
+        g = 1 - y -(y/lam)*np.abs(-y + R*x)
+        return g
+    
+    def Jacobian(x_FP, y_FP, lam): 
+            delta_y = y[1] - y[0]
+            delta_x = x[1] - x[0]
+
+            a = (f(y_FP, (x_FP  + 0.5*delta_x), lam, R, delta) - f(y_FP, (x_FP  - 0.5*delta_x), lam, R, delta))/delta_x
+            b = (f((y_FP + 0.5*delta_y), x_FP, lam, R, delta) - f((y_FP - 0.5*delta_y), x_FP, lam, R, delta))/delta_y
+            c = (g(y_FP, (x_FP  + 0.5*delta_x), lam, R) - g(y_FP, (x_FP  - 0.5*delta_x), lam, R))/delta_x
+            d = (g((y_FP + 0.5*delta_y), x_FP, lam, R) - g((y_FP - 0.5*delta_y), x_FP, lam, R))/delta_y
+
+            determinant = a*d - b*c
+            trace = a + d
+
+            return [trace, determinant]
+    
+    for j in range(len(lam)):
+    
+        # Fixed Points
+        parameter_1 = (1 - delta)*y**3 + (delta*(R + lam[j]) - (1 + lam[j]))*y**2 + lam[j]*(2 - delta)*y - lam[j]
+        parameter_sign_1 = np.sign(parameter_1)
+
+        parameter_2 = (delta - 1)*y**3 + (delta*(lam[j] - R) + (1 - lam[j]))*y**2 + lam[j]*(2 - delta)*y - lam[j]
+        parameter_sign_2 = np.sign(parameter_2)
+
+        FPs = []
+        for i in range(len(y) - 1):
+            if abs(parameter_sign_1[i] - parameter_sign_1[i + 1]) == 2: # >0 
+                y_FP = y[i] 
+                x_FP = (lam[j]/(R*y_FP))*(1 - y_FP + (1/lam[j])*y_FP**2)
+                FP = [x_FP, y_FP]
+                FPs = np.append(FPs, FP)
+            elif abs(parameter_sign_2[i] - parameter_sign_2[i + 1]) == 2: # <0
+                y_FP = y[i] 
+                x_FP = (lam[j]/(R*y_FP))*(-1 + y_FP + (1/lam[j])*y_FP**2)
+                FP = [x_FP, y_FP]
+                FPs = np.append(FPs, FP)
+
+        # Jacobian analysis for stationary points
+
+        for i in range(int(len(FPs)/2)):
+            stability = Jacobian(FPs[2*i], FPs[2*i + 1], lam[j])
+
+            # unstable spiral
+            if stability[0] > 0 and stability[1] > 0 and stability[1] > 0.25*stability[0]**2:
+                F_stable_node[j] = (R*FPs[2*i] - FPs[2*i + 1])/lam[j]
+            # stable spiral
+            if stability[0] < 0 and stability[1] > 0 and stability[1] > 0.25*stability[0]**2:
+                F_stable_spiral[j] = (R*FPs[2*i] - FPs[2*i + 1])/lam[j]
+            # unstable node
+            if stability[0] > 0 and stability[1] > 0 and stability[1] < 0.25*stability[0]**2: 
+                F_unstable_node[j] = (R*FPs[2*i] - FPs[2*i + 1])/lam[j]
+            # stable node
+            if stability[0] < 0 and stability[1] > 0 and stability[1] < 0.25*stability[0]**2: 
+                F_stable_node[j] = (R*FPs[2*i] - FPs[2*i + 1])/lam[j]
+            # unstable node
+            if stability[0] > 0 and stability[1] < 0:
+                F_unstable_node[j] = (R*FPs[2*i] - FPs[2*i + 1])/lam[j]
+            # unstable node
+            if stability[0] < 0 and stability[1] < 0:
+                F_unstable_node[j] = (R*FPs[2*i] - FPs[2*i + 1])/lam[j]
+
+    return lam, F_stable_node, F_stable_spiral, F_unstable_node
+
+#%% 
+
+y = np.linspace(0, 1, 100000)
+x = np.linspace(0, 1, 100000)
+R = 2
+delta = 1/6
+lam_vals = 200
+
+[lam, F_stable_node, F_stable_spiral, F_unstable_node] = S61B(y, x, R, delta, lam_vals)
+
+for i in range(lam_vals):
+    if round(F_stable_node[i], 15) == 0:
+        F_stable_node[i] = np.nan
+    if round(F_stable_spiral[i], 15) == 0:
+        F_stable_spiral[i] = np.nan
+    if round(F_unstable_node[i], 15) == 0:
+        F_unstable_node[i] = np.nan
+        
+plt.rcParams['figure.figsize'] = [8, 8]
+plt.plot(lam, F_stable_node, linewidth = 4, color = 'blue')
+plt.plot(lam, F_stable_spiral, linewidth = 4, color = 'green')
+plt.plot(lam, F_unstable_node, linewidth = 4, color = 'red')
+plt.axhline(0, linestyle = '--', color = 'black')
+
+# # Add other features to the bifurcation diagram
+plt.axvline(0.335, ymin = 0.505, ymax = 0.73, linewidth = 4, color = 'black')
+plt.arrow(0.335, -0.4, 0, 0.3, length_includes_head = True, head_width = 0.01, head_length = .1, color = 'black')
+
+plt.xlabel("$\lambda$", fontsize = 25)
+plt.ylabel("Flow Rate (F)", fontsize = 25)
+plt.xlim([0.1, 0.5])
+plt.ylim([-2, 1])
+plt.xticks(np.arange(0.1, 0.6, 0.1), fontsize = 25)
+plt.yticks(np.arange(-2, 2, 1), fontsize = 25)
+plt.text(0.12, 0.65, 'a)', fontsize = 40, weight='bold')
+plt.show()
